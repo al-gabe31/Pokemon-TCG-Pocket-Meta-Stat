0499efe47e82e79294cc5b06f481b949c97c46e1
Gabe Aquino <lq90575@umbc.edu>
2025-01-25 16:44:31 -0500

    updated get_move function

--- modules/database_access.py
+++ modules/database_access.py
@@ -1,7 +1,14 @@
 from utilities import *
 
 
-def get_move(move_name, column_index = -1):
+
+MOVE_DEFAULT_DESCRIPTION = ''
+MOVE_DEFAULT_ATTACK_TYPE = ''
+MOVE_DEFAULT_BASE_DAMAGE = -10
+MOVE_DEFAULT_COST = '{}'
+MOVE_DEFAULT_EFFECTS = '{"null": "null"}'
+
+def get_move(move_name, description = MOVE_DEFAULT_DESCRIPTION, attack_type = MOVE_DEFAULT_ATTACK_TYPE, base_damage = MOVE_DEFAULT_BASE_DAMAGE, cost = MOVE_DEFAULT_COST, effects = MOVE_DEFAULT_EFFECTS):
     conn = None
     cursor = None
 
@@ -9,9 +16,42 @@ def get_move(move_name, column_index = -1):
         conn = sqlite3.connect(DATABASE_PATH)
         cursor = conn.cursor()
 
-        # fetching the move in the database
-        cursor.execute(f'select * from move where move_name = \"{move_name}\" collate nocase')
-        return cursor.fetchall()[0]
+        additional_where = ''
+
+        # additing additional where clauses if parameters were provided
+        if description != MOVE_DEFAULT_DESCRIPTION:
+            additional_where += f'description = \'{description}\' and '
+        if attack_type != MOVE_DEFAULT_ATTACK_TYPE:
+            additional_where += f'attack_type = \'{attack_type}\' and '
+        if base_damage != MOVE_DEFAULT_BASE_DAMAGE:
+            additional_where += f'base_damage = {base_damage} and '
+        if cost != MOVE_DEFAULT_COST:
+            additional_where += f'cost = \'{cost}\' and '
+        if effects != MOVE_DEFAULT_EFFECTS:
+            additional_where == f'effects = \'{effects}\' and '
+
+        query = f'''
+            select *
+            from move
+            where {additional_where} move_name = '{move_name}'
+        '''
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        # no copies of the move found
+        if len(result) == 0:
+            print(f'No copies of the move {move_name} was found with the given parameters.')
+            return -1
+        
+        # multiple copies of the move found
+        if len(result) > 1:
+            print(f'{len(result)} copies of the move {move_name} was found. Please be more specific.')
+            return -1
+        
+        # found 1 copy of a move
+        else:
+            return result[0][0] # returning the move's id
     except Exception as e:
         print(f'SQL ERROR - {e}')
     finally:
@@ -19,3 +59,5 @@ def get_move(move_name, column_index = -1):
             cursor.close()
         if conn:
             conn.close()
+    
+    return -1 # move not found

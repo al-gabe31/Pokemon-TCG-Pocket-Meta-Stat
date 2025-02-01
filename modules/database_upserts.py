3a2c85164856318d3e5ac8494013c00f2e9c1f97
Gabe Aquino <lq90575@umbc.edu>
2025-01-31 19:20:53 -0500

    ready to start inserting pokemon cards

--- modules/database_upserts.py
+++ modules/database_upserts.py
@@ -25,10 +25,18 @@ MOVE_DEFAULT_BASE_DAMAGE = -10
 MOVE_DEFAULT_COST = '{}'
 MOVE_DEFAULT_EFFECTS = '{"null": "null"}'
 
-def move_upsert(move_name, description = MOVE_DEFAULT_DESCRIPTION, base_damage = MOVE_DEFAULT_BASE_DAMAGE, cost = MOVE_DEFAULT_COST, effects = MOVE_DEFAULT_EFFECTS, force_insert = False, force_unique = True, debug = False):
+def move_upsert(
+        move_name, 
+        description = MOVE_DEFAULT_DESCRIPTION, 
+        base_damage = MOVE_DEFAULT_BASE_DAMAGE, 
+        cost = MOVE_DEFAULT_COST, 
+        effects = MOVE_DEFAULT_EFFECTS, 
+        force_insert = False, 
+        force_unique = True, 
+        debug = False
+    ):
     conn = None
     cursor = None
-    cost_error = False
 
     try:
         conn = sqlite3.connect(DATABASE_PATH)
@@ -44,7 +52,6 @@ def move_upsert(move_name, description = MOVE_DEFAULT_DESCRIPTION, base_damage =
 
         if passed_cost_test == False:
             raise ValueError('Invalid cost passed. Make sure all cost types are valid')
-            return # exits the function
 
         action_type = '' # we need to know whether we're going to be inserting or updating a record into the table
         cursor.execute(f'select count(*) from move where move_name = \"{move_name}\"')
@@ -53,7 +60,7 @@ def move_upsert(move_name, description = MOVE_DEFAULT_DESCRIPTION, base_damage =
         # deciding on action type (insert, upsert, or error case)
         if force_insert == True and force_unique == True and num_counts > 0:
             # trying to insert a unique but the move already exists
-            print(f'Move {move_name} already exists in move database')
+            print(f'Move {move_name} already exists in move table')
 
             if debug:
                 move_list_copies(move_name)
@@ -67,7 +74,7 @@ def move_upsert(move_name, description = MOVE_DEFAULT_DESCRIPTION, base_damage =
             action_type = 'upsert'
         elif base_damage == MOVE_DEFAULT_BASE_DAMAGE or cost == MOVE_DEFAULT_COST or effects == MOVE_DEFAULT_EFFECTS:
             # trying to insert but not all parameters are provided
-            print('All parameters need to be populated in order to insert into move database')
+            print('All parameters need to be populated in order to insert into move table')
             return # exits the function
         else:
             # inserting new move into the table
@@ -127,7 +134,17 @@ ABILITY_DEFAULT_ACTIVE_REQ = '{"null": "null"}'
 ABILITY_DEFAULT_REPEATABLE = -1
 ABILITY_DEFAULT_EFFECTS = '{"null": "null"}'
 
-def ability_upsert(ability_name, description = ABILITY_DEFAULT_DESCRIPTION, activable = ABILITY_DEFAULT_ACTIVABLE, active_req = ABILITY_DEFAULT_ACTIVE_REQ, repeatable = ABILITY_DEFAULT_REPEATABLE, effects = ABILITY_DEFAULT_EFFECTS, force_insert = False, force_unique = True, debug = False):
+def ability_upsert(
+        ability_name, 
+        description = ABILITY_DEFAULT_DESCRIPTION, 
+        activable = ABILITY_DEFAULT_ACTIVABLE, 
+        active_req = ABILITY_DEFAULT_ACTIVE_REQ, 
+        repeatable = ABILITY_DEFAULT_REPEATABLE, 
+        effects = ABILITY_DEFAULT_EFFECTS, 
+        force_insert = False, 
+        force_unique = True, 
+        debug = False
+    ):
     conn = None
     cursor = None
 
@@ -142,7 +159,7 @@ def ability_upsert(ability_name, description = ABILITY_DEFAULT_DESCRIPTION, acti
         # deciding on action type (insert, upsert, or error case)
         if force_insert == True and force_unique == True and num_counts > 0:
             # trying to insert a unique but the ability already exists
-            print(f'Ability {ability_name} already exists in ability database')
+            print(f'Ability {ability_name} already exists in ability table')
 
             if debug:
                 ability_list_copies(ability_name)
@@ -156,7 +173,7 @@ def ability_upsert(ability_name, description = ABILITY_DEFAULT_DESCRIPTION, acti
             action_type = 'upsert'
         elif activable == ABILITY_DEFAULT_ACTIVABLE or active_req == ABILITY_DEFAULT_ACTIVE_REQ or repeatable == ABILITY_DEFAULT_REPEATABLE or effects == ABILITY_DEFAULT_EFFECTS:
             # trying to insert but not all parameters are provided
-            print('All parameters need to be populated in order to insert into ability database')
+            print('All parameters need to be populated in order to insert into ability table')
             return # exits the function
         else:
             # inserting new ability into the table
@@ -218,13 +235,115 @@ POKEMON_DEFAULT_IS_EX = -1
 POKEMON_DEFAULT_BASE_HP = -10
 POKEMON_DEFAULT_STAGE = ''
 POKEMON_DEFAULT_EVOLUTION_NAME = ''
-POKEMON_DEFAULT_MOVE_1 = -1
-POKEMON_DEFAULT_MOVE_2 = -1
-POKEMON_DEFAULT_ABILITY = -1
+POKEMON_DEFAULT_MOVE_1_ID = -1
+POKEMON_DEFAULT_MOVE_2_ID = -1
+POKEMON_DEFAULT_ABILITY_ID = -1
 POKEMON_DEFAULT_WEAKNESS = ''
 POKEMON_DEFAULT_RETREAT_COST = -1
 
-def pokemon_upsert(pokemon_name, pokemon_type, is_ex, base_hp, stage, evolution_name, move_1, move_2, ability, weakness, retreat_cost):
-    pass
+def pokemon_upsert(
+        pokemon_name, 
+        pokemon_type = POKEMON_DEFAULT_TYPE, 
+        is_ex = POKEMON_DEFAULT_IS_EX, 
+        base_hp = POKEMON_DEFAULT_BASE_HP, 
+        stage = POKEMON_DEFAULT_STAGE, 
+        evolution_name = POKEMON_DEFAULT_EVOLUTION_NAME, 
+        move_1_id = POKEMON_DEFAULT_MOVE_1_ID, 
+        move_2_id = POKEMON_DEFAULT_MOVE_2_ID, 
+        ability_id = POKEMON_DEFAULT_ABILITY_ID, 
+        weakness = POKEMON_DEFAULT_WEAKNESS, 
+        retreat_cost = POKEMON_DEFAULT_RETREAT_COST,
+        force_insert = False,
+        force_unique = True,
+        debug = False,
+        display_id_names = False
+    ):
+    conn = None
+    cursor = None
+
+    try:
+        conn = sqlite3.connect(DATABASE_PATH)
+        cursor = conn.cursor()
+
+        action_type = '' # we need to know whether we're going to be inserting or updating a record into the table
+        cursor.execute(f'select count(*) from pokemon_card where pokemon_name = \"{pokemon_name}\"')
+        num_counts = cursor.fetchall()[0][0]
+
+        # deciding on action type (insert, upsert, error case)
+        if force_insert == True and force_unique == True and num_counts > 0:
+            # trying to insert a unique but the pokemon already exists
+            print(f'Pokemon {pokemon_name} already exists in pokemon_card table')
+
+            if debug:
+                pokemon_card_list_copies(pokemon_name, display_id_names)
+            return # exits the function
+        elif force_insert == False and force_unique == True and num_counts > 1:
+            # trying to modify a pokemon but there are multiple copies
+            print(f'Pokemon {pokemon_name} has {num_counts} copies! Use pokemon_card_modify function to edit a specific one instead.')
+            return # exits the function
+        elif force_insert == False and force_unique == True and num_counts == 1:
+            # updating the only existing copy of a pokemon
+            action_type = 'upsert'
+        elif pokemon_type == POKEMON_DEFAULT_TYPE or is_ex == POKEMON_DEFAULT_IS_EX or base_hp == POKEMON_DEFAULT_BASE_HP or stage == POKEMON_DEFAULT_STAGE or evolution_name == POKEMON_DEFAULT_EVOLUTION_NAME or weakness == POKEMON_DEFAULT_WEAKNESS or retreat_cost == POKEMON_DEFAULT_RETREAT_COST:
+            # trying to insert but not all parameters are provided
+            print('All parameters need to be populated in order to insert into pokemon_card table')
+            return # exits the function
+        else:
+            # inserting a new pokemon_card into the table
+            action_type = 'insert'
+
+        # verifying that evolution_name is in pokemon_card table
+        cursor.execute(f'select count(*) from pokemon_card where pokemon_name = \"{evolution_name}\"')
+        evolution_matches = cursor.fetchall()[0][0]
+
+        # error only if there were no evolution matches & evolution parameter was provided (ie not equal to default evolution)
+        if evolution_matches == 0 and evolution_name != POKEMON_DEFAULT_EVOLUTION_NAME:
+            print(f'Evolution pokemon {evolution_name} not found')
+            return # exits the function
+
+        if action_type == 'insert':
+            # we have to insert into card & pokemon_card table
+            # inserting into card table
+            cursor.execute(f'''
+                insert into card(card_name, card_archetype)
+                values(\'{pokemon_name}\', 'pokemon')
+            ''')
+
+            card_id = cursor.lastrowid
+
+            # verifying that card_id is not None
+            if card_id is None:
+                raise ValueError('card_id is None')
+
+            # inserting into pokemon_card table
+            additional_insert = ''
+            additional_values = ''
+
+            # reminder that move_1, move_2, & ability can be null (parameter will be passed as -1)
+            # for any that aren't null, include it in the insert statement
+            if move_1_id != POKEMON_DEFAULT_MOVE_1_ID:
+                additional_insert += ', move_1_id'
+                additional_values += f', {move_1_id}'
+            if move_2_id != POKEMON_DEFAULT_MOVE_1_ID:
+                additional_insert += ', move_2_id'
+                additional_values += f', {move_2_id}'
+            if ability_id != POKEMON_DEFAULT_ABILITY_ID:
+                additional_insert += ', ability_id'
+                additional_values += f', {ability_id}'
+
+            cursor.execute(f'''
+                insert into pokemon_card(card_id, pokemon_name, pokemon_type, is_ex, base_hp, stage, evolution_name, weakness, retreat_cost{additional_insert})
+                values({card_id}, '{pokemon_name}', '{pokemon_type}', {is_ex}, {base_hp}, '{stage}', '{evolution_name}', '{weakness}', {retreat_cost}{additional_values})
+            ''')
+
+        conn.commit() # committing transaction
+    except Exception as e:
+        print(f'SQL ERROR - {e}')
+        conn.rollback() # rolling back transaction
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
 
 # END OF FILE
--- modules/utilities.py
+++ modules/utilities.py
@@ -79,3 +79,57 @@ def ability_list_copies(ability_name):
             cursor.close()
         if conn:
             conn.close()
+
+def pokemon_card_list_copies(pokemon_name, display_id_names = False):
+    conn = None
+    cursor = None
+
+    try:
+        conn = sqlite3.connect(DATABASE_PATH)
+        cursor = conn.cursor()
+
+        # getting the number of copies from the pokemon_name database
+        cursor.execute(f'select * from pokemon_card where pokemon_name = \"{pokemon_name}\"')
+        copies = cursor.fetchall()
+        num_counts = len(copies)
+
+        # there are no copies of the pokemon
+        if num_counts == 0:
+            print(f'No copies of {pokemon_name} found in pokemon_name database')
+            return
+        
+        # printing all copies of the current pokemon
+        for pokemon in copies:
+            pokemon_id, card_id, pokemon_name, pokemon_type, is_ex, base_hp, stage, evolution_name, move_1_id, move_2_id, ability_id, weakness, retreat_cost = pokemon
+
+            # we can display the move/ability names instead of the IDs
+            if display_id_names:
+                move_1_name, move_2_name, ability_name = 'None'
+
+                # getting move 1 name
+                if move_1_id != -1:
+                    cursor.execute(f'select move_name from move where move_id = {move_1_id}')
+                    move_1_name = cursor.fetchall()[0][0]
+                
+                # getting move 2 name
+                if move_2_id != -1:
+                    cursor.execute(f'select move_name from move where move_id = {move_2_id}')
+                    move_2_name = cursor.fetchall()[0][0]
+
+                # getting ability name
+                if ability_id != -1:
+                    cursor.execute(f'select ability_name from ability where ability_id = {ability_id}')
+                    ability_name = cursor.fetchall()[0][0]
+
+                # printing our information
+                print(f'[POKEMON_ID # {pokemon_id}] {pokemon_name}\n\tCard ID: {card_id}\n\tPokemon Type: {pokemon_type}\n\tIs EX: {is_ex}\n\tBase HP: {base_hp}\n\tStage: {stage}\n\tEvolution Name: {evolution_name}\n\Move 1: {move_1_name   }\n\tMove 2: {move_2_name}\n\tAbility: {ability_name}\n\tWeakness: {weakness}\n\tRetreat Cost: {retreat_cost}\n')
+                return
+
+            print(f'[POKEMON_ID # {pokemon_id}] {pokemon_name}\n\tCard ID: {card_id}\n\tPokemon Type: {pokemon_type}\n\tIs EX: {is_ex}\n\tBase HP: {base_hp}\n\tStage: {stage}\n\tEvolution Name: {evolution_name}\n\tMove 1 ID: {move_1_id}\n\tMove 2 ID: {move_2_id}\n\tAbility ID: {ability_id}\n\tWeakness: {weakness}\n\tRetreat Cost: {retreat_cost}\n')
+    except Exception  as e:
+        print(f'SQL ERROR - {e}')
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
